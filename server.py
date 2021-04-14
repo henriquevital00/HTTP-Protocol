7fecb937defb5aa17ea5f4eba232bc9b621acba3
henriquevital00 <henriquevital00@gmail.com>
2021-04-13 22:45:23 -0300

    Fixed server

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from socket import *
 import sys
 import os
 
-HOST = '127.0.0.1'
+HOST = ''
 PORT = 10000
 
 
@@ -13,31 +13,29 @@ def createServer():
 
     print("O sevidor está pronto para receber!")
 
-    while True:
-        try:
-            while (1):
-                conn, addr = tcp.accept()
-                pid = os.fork()
-                if pid == 0:
-                    now = datetime.now()
-                    tcp.close()
-                    print("Conectado por {} na porta {}".format(
-                        addr[0], addr[1]))
-                    data = conn.recv(2048)
-                    data = "HTTP/1.1 200 OK\r\n"
-                    data += "Content-Type: text/html; charset=utf-8\r\n"
-                    data += "\r\n"
-                    data += "<html><body>Hello World!</body></html>\r\n\r\n"
-                    clientsocket.sendall(data.encode())
-                    clientsocket.shutdown(SHUT_WR)
-                    con.send(b'The server is off')
-                    conn.close()
-                    print("Finalizando conexao do cliente", addr)
-                    print("Access http://localhost:9000")
-                else:
-                    conn.close()
-        except:
-            print("Deu ruim emu irmao")
+    try:
+        while True:
+            conn, addr = tcp.accept()
+            #pid = os.fork()
+            #if pid == 0:
+            tcp.close()
+            print("Conectado por {} na porta {}".format(addr[0], addr[1]))
+            data = "HTTP/1.1 200 OK\r\n"
+            data += "Content-Type: text/html; charset=utf-8\r\n"
+            data += "\r\n"
+            data += "<html><body>Hello World!</body></html>\r\n\r\n"
+            conn.sendall(data.encode())
+            conn.shutdown(SHUT_WR)
+            conn.send(b'The server is off')
+            conn.close()
+            print("Finalizando conexao do cliente", addr)
+            print("Access http://localhost:9000")
+        #else:
+        #    conn.close()
+    except Exception as e:
+        print(e)
+        print("Deu ruim emu irmao")
+        conn.close()
 
 
 createServer()
