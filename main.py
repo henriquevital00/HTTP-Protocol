a56b676e021c171a697b22699286173274ab8515
henriquevital00 <henriquevital1000@hotmail.com>
2021-04-17 19:24:21 -0300

    Fixed server script

---
+++ main.py
@@ -0,0 +1,10 @@
+from server import Server
+
+
+def main():
+    server = Server()
+    server.createServer()
+
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -2,34 +2,25 @@ from socket import *
 import sys
 import os
 
-HOST = ''
-PORT = 10000
 
+class Server(object):
+    def __init__(self):
+        self.HOST = ''
+        self.PORT = 9000
 
-def createServer():
-    with socket(AF_INET, SOCK_STREAM) as sock:
-        sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        sock.bind((HOST, PORT))
-        sock.listen(5)
-        conn, addr = sock.accept()
-
-        print("Conectado por {} na porta {}".format(addr[0], addr[1]))
-        data = "HTTP/1.1 200 OK\r\n"
-        data += "Content-Type: text/html; charset=utf-8\r\n"
-        data += "\r\n"
-        data += "<html><body>Hello World!</body></html>\r\n\r\n"
-        conn.send(data.encode())
-
-        #try:
-        #    while True:
-        #        data = sock.recv(1024).decode()
-        #        print("Finalizando conexao do cliente", addr)
-        #        print("Access http://localhost:10000")
-
-        #except Exception as e:
-        #    print(e)
-        #    print("Deu ruim emu irmao")
-        sock.close()
-
-
-createServer()
+    def createServer(self):
+        with socket(AF_INET, SOCK_STREAM) as sock:
+            sock.bind((self.HOST, self.PORT))
+            sock.listen(5)
+            while True:
+                conn, addr = sock.accept()
+                print("Conectado por {} na porta {}".format(addr[0], addr[1]))
+                pid = os.fork()
+                if pid == 0:
+                    data = "HTTP/1.1 200 OK\r\n"
+                    data += "Content-Type: text/html; charset=utf-8\r\n"
+                    data += "\r\n"
+                    data += "<html><body>Hernique fala y  fala!</body></html>\r\n\r\n"
+                    conn.send(data.encode())
+                else:
+                    conn.close()
