8c2150a159ec17d316de22a397e081832acb8f3f
henriquevital00 <henriquevital1000@hotmail.com>
2021-04-17 17:48:30 -0300

    Fixed server script

--- server.py
+++ server.py
@@ -7,35 +7,29 @@ PORT = 10000
 
 
 def createServer():
-    tcp = socket(AF_INET, SOCK_STREAM)
-    tcp.bind((HOST, PORT))
-    tcp.listen(1)
+    with socket(AF_INET, SOCK_STREAM) as sock:
+        sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        sock.bind((HOST, PORT))
+        sock.listen(5)
+        conn, addr = sock.accept()
 
-    print("O sevidor está pronto para receber!")
+        print("Conectado por {} na porta {}".format(addr[0], addr[1]))
+        data = "HTTP/1.1 200 OK\r\n"
+        data += "Content-Type: text/html; charset=utf-8\r\n"
+        data += "\r\n"
+        data += "<html><body>Hello World!</body></html>\r\n\r\n"
+        conn.send(data.encode())
 
-    try:
-        while True:
-            conn, addr = tcp.accept()
-            pid = os.fork()
-            if pid == 0:
-                tcp.close()
-                print("Conectado por {} na porta {}".format(addr[0], addr[1]))
-                data = "HTTP/1.1 200 OK\r\n"
-                data += "Content-Type: text/html; charset=utf-8\r\n"
-                data += "\r\n"
-                data += "<html><body>Hello World!</body></html>\r\n\r\n"
-                conn.sendall(data.encode())
-                conn.shutdown(SHUT_WR)
-                conn.send(b'The server is off')
-                conn.close()
-                print("Finalizando conexao do cliente", addr)
-                print("Access http://localhost:9000")
-            else:
-                conn.close()
-    except Exception as e:
-        print(e)
-        print("Deu ruim emu irmao")
-        conn.close()
+        #try:
+        #    while True:
+        #        data = sock.recv(1024).decode()
+        #        print("Finalizando conexao do cliente", addr)
+        #        print("Access http://localhost:10000")
+
+        #except Exception as e:
+        #    print(e)
+        #    print("Deu ruim emu irmao")
+        sock.close()
 
 
 createServer()
