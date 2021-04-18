5bdae25780cc1c0233d5b5f5f79ffde6ddb0b254
henriquevital00 <henriquevital1000@hotmail.com>
2021-04-17 22:46:03 -0300

    Created new scripts

--- server.py
+++ server.py
@@ -1,26 +1,41 @@
 from socket import *
-import sys
-import os
+from Gateway import HOST, PORT
+from http import HTTP
+from pdb import set_trace
 
 
-class Server(object):
+class Server:
     def __init__(self):
-        self.HOST = ''
-        self.PORT = 9000
+        pass
 
-    def createServer(self):
+    def get_page(self, page):
+        try:
+            with open(page) as content:
+                data = content.read()
+                return HTTP.GET_Response(data)
+        except Exception as e:
+            print("Deu merda parceiro")
+
+    def parse_request(self, data):
+        return data.split(' ')[1][1:]
+
+    def test(self):
         with socket(AF_INET, SOCK_STREAM) as sock:
-            sock.bind((self.HOST, self.PORT))
+            sock.bind((HOST, PORT))
             sock.listen(5)
             while True:
                 conn, addr = sock.accept()
-                print("Conectado por {} na porta {}".format(addr[0], addr[1]))
-                pid = os.fork()
-                if pid == 0:
-                    data = "HTTP/1.1 200 OK\r\n"
-                    data += "Content-Type: text/html; charset=utf-8\r\n"
-                    data += "\r\n"
-                    data += "<html><body>Hernique fala y  fala!</body></html>\r\n\r\n"
-                    conn.send(data.encode())
-                else:
-                    conn.close()
+                data = conn.recv(4024)
+                print(data.decode())
+                page = self.parse_request(data.decode())
+                print("PAGE: ", page)
+                data = self.get_page(page)
+                conn.sendall(data.encode())
+
+
+def main():
+    server = Server()
+    server.test()
+
+
+main()
