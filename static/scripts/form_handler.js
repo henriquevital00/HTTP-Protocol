275181948684a822cf1a64e04059b9ada0fe2b8e
henriquevital00 <henriquevital1000@hotmail.com>
2021-04-26 22:42:16 -0300

    Added publications card

---
+++ static/scripts/form_handler.js
@@ -0,0 +1,41 @@
+const form = document.getElementById("needs-validation");
+
+const FormDatatoJson = formData => {
+
+    obj = {};
+    formData.forEach((value, key) => obj[key] = value);
+    obj["date"] = new Date(Date.now()).toISOString()
+
+    return JSON.stringify(obj);
+}
+
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    formData = new FormData(e.target);
+    result_data = FormDatatoJson(formData);
+
+    fetch("http://localhost:8000/publications/create",{
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'text/json'
+        },
+        method: "POST",
+        body: result_data
+    })
+    .then(
+        response => response.json()
+            .then(response => {
+                response = response.reverse().map(resp =>
+                    `<div class="publication-card mt-4">
+                        <h1>${resp.hashtag}</h1>
+                        <p>${resp.content}</p>
+                        <p>${resp.date}</p>
+                    </div>`
+                );
+
+                $("#posts").html(response.join(""));
+            })
+    )
+})
