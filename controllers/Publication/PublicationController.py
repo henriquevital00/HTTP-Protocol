04c196d8bde3123c3fbf709fd202c54e99a0374b
henriquevital00 <henriquevital1000@hotmail.com>
2021-05-01 00:23:34 -0300

    Implemented delete

---
+++ controllers/Publication/PublicationController.py
@@ -0,0 +1,57 @@
+import json
+
+
+class PublicationController:
+    def __init__(self):
+        self.file_name = 'controllers/Publication/publications.json'
+
+    def create(self, sent_data):
+        with open(self.file_name, "r+") as file:
+            data = json.load(file)
+            sent_data = json.loads(sent_data)
+            sent_data["id"] = data[-1]["id"] + 1
+            data.append(sent_data)
+            file.seek(0)
+            json.dump(data, file, indent=4)
+            return json.dumps(data)
+
+    def findById(self, id):
+        try:
+            with open(self.file_name, 'r') as json_file:
+                data = json.load(json_file)
+                for publication in data:
+                    if publication["id"] == id:
+                        return publication
+        except:
+            print('Error searching in json file')
+
+    def findAll(self):
+        with open(self.file_name, 'r+') as file:
+            data = json.load(file)
+            file.seek(0)
+
+            return json.dumps(data)
+
+    def removeData(self, Id: str) -> None:
+        try:
+            with open(self.file_name, 'r') as data_file:
+                data = json.load(data_file)
+            if Id in data:
+                del data[Id]
+            with open(self.file_name, 'w') as data_file:
+                data = json.dump(data, data_file)
+        except:
+            print("Can not remove postagem")
+
+    def deleteById(self, Id: int) -> None:
+        try:
+            with open(self.file_name, 'r') as data_file:
+                data = json.load(data_file)
+            for i in range(len(data)):
+                if data[i]['id'] == Id:
+                    del data[i]
+                    break
+            with open(self.file_name, 'w') as data_file:
+                data = json.dump(data, data_file)
+        except:
+            print("Can not remove postagem")
